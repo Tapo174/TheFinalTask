144cbf773fc045ae59155c5c75fe141a98f8875b
Vladimir <Vladimir08122000@gmail.com>
2022-11-30 14:45:16 +0500

    step 2

--- Program.cs
+++ Program.cs
@@ -1,3 +1,10 @@
 ﻿string[] array1 = new string[4] {"hello", "2", "world", ":-)"};
 string[] array2 = new string[array1.Length];
 
+CorrectArray(array1, array2);
+PrintArray(array2);
+
+void CorrectArray(string[] array1, string[] array2)
+{
+    
+}
